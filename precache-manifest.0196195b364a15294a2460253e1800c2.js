fda6a20de8fb9a56c47d52db3c8f09591a6a60e0
Ralabdulwa98 <rmalabdulwahab@gmail.com>
2020-04-23 13:21:35 +0300

    Updates

---
+++ precache-manifest.0196195b364a15294a2460253e1800c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b927596de5eeb8d440eefe6a8da7d4c",
+    "url": "/homathon_test/index.html"
+  },
+  {
+    "revision": "169c9a75f1372043d475",
+    "url": "/homathon_test/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "1febeb413a3fe9a4bb51",
+    "url": "/homathon_test/static/css/main.064259ec.chunk.css"
+  },
+  {
+    "revision": "169c9a75f1372043d475",
+    "url": "/homathon_test/static/js/2.4f724f26.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/homathon_test/static/js/2.4f724f26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1febeb413a3fe9a4bb51",
+    "url": "/homathon_test/static/js/main.5065a982.chunk.js"
+  },
+  {
+    "revision": "7c866afeae5923989cb3",
+    "url": "/homathon_test/static/js/runtime-main.1199e71d.js"
+  }
+]);
