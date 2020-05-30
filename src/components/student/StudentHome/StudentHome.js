cbafd7e3f934948f03de59010ef88f86d6fc0553
vex-xcc <vex.xcc@gmail.com>
2020-05-30 10:09:33 +0300

    add bg image

--- src/components/student/StudentHome/StudentHome.js
+++ src/components/student/StudentHome/StudentHome.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
 import './StudentHome.css'
 import Activities from "../activities/activities"
-
+import sport from '../../../images/run.png'
+import ed from '../../../images/ScienceIdeas.jpg'
+import en from '../../../images/download.jpeg'
+import unn from '../../../images/unnamed.jpg'
 export default class StudentHome extends Component {
    constructor(props) {
       super(props);
@@ -32,25 +35,25 @@ export default class StudentHome extends Component {
             {this.state.toggle === false?
             <div className="contener">
 
-               <div className="card" onClick= {()=>this.typehandler("التعليم" , "Education") }>
+               <div className="card" style={{backgroundImage: `url(${ed})` }} onClick= {()=>this.typehandler("التعليم" , "Education") }>
                   <div className="icon"><i className="material-icons md-36">local_library</i></div>
                   <p className="title">التعليم</p>
                   <p className="text"> أطلب من العلوم علماً ينفعك ينفي الأذى والعيب ثم يرفعك</p>
                </div>
 
-               <div className="card" onClick= {()=>this.typehandler("الثقافة" , "Cultural") }>
+               <div className="card" style={{backgroundImage: `url(${unn})` }} onClick= {()=>this.typehandler("الثقافة" , "Cultural") }>
                   <div className="icon"><i className="material-icons md-36">emoji_objects</i></div>
                   <p className="title">الثقافة</p>
                   <p className="text">الأمة هوية، والهوية ثقافة، والثقافة دين، ولسان، ووجدان</p>
                </div>
 
-               <div className="card" onClick= {()=>this.typehandler("الترفيه" , "Entertainment") }>
+               <div className="card" style={{backgroundImage: `url(${en})` }} onClick= {()=>this.typehandler("الترفيه" , "Entertainment") }>
                   <div className="icon"><i className="material-icons md-36">sentiment_very_satisfied</i></div>
                   <p className="title">الترفيه</p>
                   <p className="text">انقر للمزيد من السعادة.</p>
                </div>
 
-               <div className="card" onClick= {()=>this.typehandler("الرياضة" , "Sport") }>
+               <div className="card" style={{backgroundImage: `url(${sport})` }} onClick= {()=>this.typehandler("الرياضة" , "Sport") }>
                   <div className="icon"><i className="material-icons md-36">directions_run</i></div>
                   <p className="title">الرياضة</p>
                   <p className="text"> إن الرياضة مصنع العقلاء .</p>
