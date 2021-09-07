606a7b279c423f398e5e906b7c36379e80731780
Omar Alhazmi <vex.xcc@gmail.com>
2021-09-07 17:25:19 +0300

    Updates

---
+++ precache-manifest.dd2ee0e84dcbc1fd1b6c1b2758eefc24.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab63b493e76c8606ebada5e880620731",
+    "url": "/MedCB_Platform/index.html"
+  },
+  {
+    "revision": "608d8ef960719b357b23",
+    "url": "/MedCB_Platform/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "745dd88d8880ffeec79b",
+    "url": "/MedCB_Platform/static/css/main.ac54c4c9.chunk.css"
+  },
+  {
+    "revision": "608d8ef960719b357b23",
+    "url": "/MedCB_Platform/static/js/2.457bdc4c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MedCB_Platform/static/js/2.457bdc4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "745dd88d8880ffeec79b",
+    "url": "/MedCB_Platform/static/js/main.d0f4c280.chunk.js"
+  },
+  {
+    "revision": "ef8862bbbd30cd6d1912",
+    "url": "/MedCB_Platform/static/js/runtime-main.d0f447b8.js"
+  },
+  {
+    "revision": "98ea4825c863736e9b60776955f0fd36",
+    "url": "/MedCB_Platform/static/media/cl.98ea4825.png"
+  },
+  {
+    "revision": "3d83aaf37bb18027339866916094f7fa",
+    "url": "/MedCB_Platform/static/media/download.3d83aaf3.jpeg"
+  },
+  {
+    "revision": "cd3cfe7afe91ee165d99db861a6ced78",
+    "url": "/MedCB_Platform/static/media/edu.cd3cfe7a.png"
+  },
+  {
+    "revision": "debd74067cfd6ab3235798d1f616a1f7",
+    "url": "/MedCB_Platform/static/media/spo.debd7406.png"
+  }
+]);
